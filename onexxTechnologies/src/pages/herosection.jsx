11445b9383ee51803eaa29ec4aa56e80d6da198d
NityaCoding-gif <mynewview18@gmail.com>
2025-11-24 23:18:56 +0530

    commit added wave at top

--- onexxTechnologies/src/pages/herosection.jsx
+++ onexxTechnologies/src/pages/herosection.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import AuroraBackground from "./Aurorabackground";
 import "./AuroraBackground.css"
+import NeonWavesCenterGlow from "./wavetest";
 
 const HeroSection = () => {
   return (
@@ -16,22 +17,23 @@ const HeroSection = () => {
         amplitude={1.0}
         speed={0.5}
       />
-
-
-      <div className="relative w-full text-center px-6">
+      <div className="relative w-full text-center px-6 -mb-[34%]">
         <div className="mx-auto max-w-3xl mx-[2%] sm:mx-0">
-          <h1
-            className="relative text-4xl sm:text-5xl md:text-6xl font-bold glow-layer"
-          >
+          <h1 className="relative text-4xl sm:text-5xl md:text-6xl font-bold glow-layer">
             Innovative Solution To <br /> Accelerate Your Business
           </h1>
 
           <p className="mt-6 text-lg sm:text-xl glow-layer2">
-            Customize every element with <br/>optun’s intuitive design tools
+            Customize every element with <br />optun’s intuitive design tools
             and robust editing options.
           </p>
         </div>
       </div>
+
+      <div className="-mt-10 sm:-mt-16 md:-mt-20">
+        <NeonWavesCenterGlow />
+      </div>
+
 
 
     </>
