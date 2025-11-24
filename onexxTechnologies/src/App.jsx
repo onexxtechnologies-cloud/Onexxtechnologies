902b720b302f36a23427a3a65ad3b806c2f4653c
Megha-patel1203 <megha1203patel@gmail.com>
2025-11-24 12:04:57 +0530

    commit

--- onexxTechnologies/src/App.jsx
+++ onexxTechnologies/src/App.jsx
@@ -1,11 +1,7 @@
 import HeroSection from "./pages/herosection.jsx";
 import Navbar from "./pages/navbar.jsx";
 import Halfmoon from "./pages/halfmoon.jsx";
-<<<<<<< HEAD
-import AboutUs from "./pages/aboutus.jsx";
-=======
 import OnexxatronModel from "./pages/3dmodel.jsx"
->>>>>>> 1b2845b7dec257ab41565f38cce7cf3c788d9193
 function App() {
   return (
     <>
@@ -13,11 +9,7 @@ function App() {
       
       <HeroSection />
       <Halfmoon />
-<<<<<<< HEAD
-      <AboutUs />
-=======
       <OnexxatronModel/>
->>>>>>> 1b2845b7dec257ab41565f38cce7cf3c788d9193
     </>
   );
 }
--- onexxTechnologies/src/pages/navbar.jsx
+++ onexxTechnologies/src/pages/navbar.jsx
@@ -366,7 +366,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 nav-glass shawow-lg w-full flex justify-center py-3 md:py-4 bg-transparent">
+    <header className=" fixed top-0 left-0 right-0 z-50 transition-all duration-300 nav-glass shawow-lg w-full flex justify-center py-3 md:py-4 bg-transparent">
       <div
         className={`
           px-4 md:px-8 py-3 md:py-4 h-[64px] md:h-[70px]
