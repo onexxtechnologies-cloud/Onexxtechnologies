a46c157cb947b0ed8703892e5cf827d031e06c62
Manavpatel99 <112459650+Manavpatel99@users.noreply.github.com>
2025-11-24 10:49:17 +0530

    Update App.jsx

--- onexxTechnologies/src/App.jsx
+++ onexxTechnologies/src/App.jsx
@@ -1,15 +1,23 @@
 import HeroSection from "./pages/herosection.jsx";
 import Navbar from "./pages/navbar.jsx";
 import Halfmoon from "./pages/halfmoon.jsx";
+<<<<<<< HEAD
 import AboutUs from "./pages/aboutus.jsx";
+=======
+import OnexxatronModel from "./pages/3dmodel.jsx"
+>>>>>>> 1b2845b7dec257ab41565f38cce7cf3c788d9193
 function App() {
   return (
     <>
-
       <Navbar />
+      
       <HeroSection />
       <Halfmoon />
+<<<<<<< HEAD
       <AboutUs />
+=======
+      <OnexxatronModel/>
+>>>>>>> 1b2845b7dec257ab41565f38cce7cf3c788d9193
     </>
   );
 }
