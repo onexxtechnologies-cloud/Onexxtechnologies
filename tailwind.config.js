a8a9a211d69ef6e4e2f08430bd07e65209a42e2b
Manavpatel99 <112459650+Manavpatel99@users.noreply.github.com>
2025-12-07 12:28:18 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",                 // your main HTML
-    "./src/**/*.{js,ts,jsx,tsx}",  // all React components/pages
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
